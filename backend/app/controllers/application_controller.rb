8f3e53c9df845d360e73189bae47311e4c1e02c2
hokageCV <chaitanyavaru@gmail.com>
2024-07-12 01:07:46 +0530

    Configure to send/receive json response by default

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::API
+  respond_to :json
+  include ActionController::MimeResponds
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, defaults: { format: :json }
 
   resources :tasks
   get "up" => "rails/health#show", as: :rails_health_check
