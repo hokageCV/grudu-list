cdcbbf913a99d4fd0f63eb48a8879500193e150a
hokageCV <chaitanyavaru@gmail.com>
2024-07-13 14:15:39 +0530

    Configure devise jwt

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_11_180125) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_13_084047) do
   create_table "tasks", force: :cascade do |t|
     t.string "name", null: false
     t.boolean "completed", default: false, null: false
@@ -28,7 +28,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_11_180125) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "jti", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
