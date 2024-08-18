76a6096d5446ece0c861ae70b7bf1ada04ab1425
hokageCV <chaitanyavaru@gmail.com>
2024-08-18 17:33:10 +0530

    Configure database cleaner

---
+++ backend/spec/support/database_cleaner.rb
@@ -0,0 +1,19 @@
+RSpec.configure do |config|
+
+  config.before(:suite) do
+    # truncate the database
+    DatabaseCleaner.clean_with :truncation, except: %w(ar_internal_metadata)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
