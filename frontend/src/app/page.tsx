7e28f8b1364b1e75dda1d78628bd45c198e3a487
Mohak Chauhan <mohak46812@gmail.com>
2024-08-20 20:30:23 +0530

    initial setup

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,14 +1,12 @@
+import HomePage from '../components/Home';
+
 export default function Home() {
   return (
     <main>
       <div className='hero min-h-screen bg-base-200'>
         <div className='hero-content text-center'>
           <div className='max-w-md'>
-            <h1 className='text-5xl font-bold'>Template</h1>
-            <p className='py-6'>
-              Pokem ipsum dolor sit amet in a world we must defend make it double Skitty Ekans Aron Seel. Consectetur adipisicing elit Swift Ducklett Glaceon Rayquaza Tirtouga Sawsbuck. Fire Red Slaking Solrock Leech Seed Sapphire Ferroseed Kyurem. Wing Attack Lopunny Tynamo Donphan Dome Fossil Misdreavus Luxio. Pokemon The Movie 2000 we're blasting off again Linoone Meloetta Blue Seismitoad Venomoth.
-            </p>
-            <button className='btn btn-primary'>Get Started</button>
+            <HomePage/>
           </div>
         </div>
       </div>
