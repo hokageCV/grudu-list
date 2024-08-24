9e27087d48fdfb5d8e023452eeba403afbd8fcf1
hokageCV <chaitanyavaru@gmail.com>
2024-08-24 13:40:17 +0530

    Update collection with member routes

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -72,6 +72,9 @@ Rails.application.configure do
   # Highlight code that enqueued background job in logs.
   config.active_job.verbose_enqueue_logs = true
 
+  # Add controller & action name with sql query
+  # config.active_record.query_log_tags_enabled = true
+  # config.active_record.query_log_tags = [ :controller, :action ]
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
