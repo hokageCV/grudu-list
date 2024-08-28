2a6f68bb21f64eb8a38f88470ea19cb06446f72c
Mohak Chauhan <mohak46812@gmail.com>
2024-08-28 23:53:45 +0530

    add: create and view groups routes

---
+++ frontend/src/app/group/page.tsx
@@ -0,0 +1,11 @@
+import AllGroups from "@/components/AllGroups"
+import Navbar from "@/components/Navbar"
+
+export default function page() {
+  return (
+    <div>
+      <Navbar/>
+      <AllGroups/>
+    </div>
+  )
+}
